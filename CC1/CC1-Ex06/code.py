67195391430c623ed51caabf758b4428e9503c9e
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-06 13:36:24 +0000

    Revised CC1-Ex06.

--- CC1/CC1-Ex06/code.py
+++ CC1/CC1-Ex06/code.py
@@ -1,3 +1,3 @@
-"occupation" = x #Define a variable named occupation and set it equal to any string
-"score" = 9#Define a variable named score and set it equal to any int
-"price" = 10.00#Define a variable named price and set it equal to any float
+occupation = 'Truck Driver'
+score = int(9)
+price = float(10)
