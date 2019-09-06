f13f99680526432a2e7699e9abdda0cbb70cec97
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-06 14:03:58 +0000

    Revised CC1-Ex08.

--- CC1/CC1-Ex08/code.py
+++ CC1/CC1-Ex08/code.py
@@ -1,3 +1,4 @@
-mph = float(50) # your code here that gets user input
-kph = 1.609 * mph # your code here
+mph = input("Miles per hour? ") # your code here that gets user input
+kph = 1.609 * float(mph) # your code here
 print('Kilometers per hour:', kph)  #fill in blank
+
