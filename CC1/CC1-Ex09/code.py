765c597c18bd9101bba5d03655d3e2922c157a8a
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-06 03:52:41 +0000

    Revised CC1-Ex09.

--- CC1/CC1-Ex09/code.py
+++ CC1/CC1-Ex09/code.py
@@ -1,5 +1,4 @@
 hours = input('Enter number of hours worked: ')
 rate = input('Enter rate of pay: ')
-# Add your code below
 pay = int(hours) * int(rate)
-print("Pay" + ":", pay)  
+print("Pay" + ":", float(pay))  
