9a0017cab71a0a4b51e64fea8c8e967bc0f3879d
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-11 14:33:07 +0000

    Commpleted CC1-Ex15.

--- CC1/CC1-Ex15/code.py
+++ CC1/CC1-Ex15/code.py
@@ -10,6 +10,6 @@ pizzas = [
 selected_pizza = pizzas[randint(0, 5)]
 # DO NOT TOUCH ============================================
 
-pizza_type = selected_pizza________  # fill in blank 
+pizza_type = selected_pizza[8:]  # fill in blank 
 
 print(pizza_type)
