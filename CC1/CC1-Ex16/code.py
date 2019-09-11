36ce60e85d7d589e0543e2b0b00e398ea28e095d
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-11 13:33:41 +0000

    Completed CC1-Ex16.

--- CC1/CC1-Ex16/code.py
+++ CC1/CC1-Ex16/code.py
@@ -1,4 +1,4 @@
 str_a = 'Simple'
 str_b = 'complex'
-str_c = str_a + ______ + str_b  # Fill in the blank
+str_c = str_a + ' is better than ' + str_b  # Fill in the blank
 print(str_c)  # Should print: Simple is better than complex
