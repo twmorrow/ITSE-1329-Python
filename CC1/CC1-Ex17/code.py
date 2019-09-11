fc4e0e13afeb4cecaf0d619bea6c06cfb9ff28f0
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-11 13:39:59 +0000

    Completed CC1-Ex17.

--- CC1/CC1-Ex17/code.py
+++ CC1/CC1-Ex17/code.py
@@ -1,4 +1,4 @@
 str_a = 'Now'
 str_b = 'never'
-str_c = _____________  # Fill in the blank
+str_c = str_a + ' is better than ' + str_b  # Fill in the blank
 print(str_c)  # Should print: Now is better than never
