17d4ee4491ac1a5707839721214b9e9afd8a659a
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-13 12:43:53 +0000

    Completed CC1-Ex25.

--- CC1/CC1-Ex25/code.py
+++ CC1/CC1-Ex25/code.py
@@ -1,3 +1,3 @@
 zen_of_python = 'Namespaces are one honking great idea -- lets do more of those!'
-pos = zen_of_python.find(____)  # Fill in the blank
+pos = zen_of_python.find('-')  # Fill in the blank
 print(pos)
