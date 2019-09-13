a63ebcdd14b8e8c41611d68dd67356a0f6c5df60
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-13 12:30:20 +0000

    Completed CC1-Ex22.

--- CC1/CC1-Ex22/code.py
+++ CC1/CC1-Ex22/code.py
@@ -1,3 +1,3 @@
 zen = 'If the implementation is python to explain, it is a bad idea.'
-new_zen = zen.replace(________)  # replace python with hard
+new_zen = zen.replace('python', 'hard')  # replace python with hard
 print(new_zen)
