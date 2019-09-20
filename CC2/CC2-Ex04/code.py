aa49af7d9e523778e89ab2ef8942dd4ade9134e6
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-20 13:53:52 +0000

    Completed CC2-Ex04.

--- CC2/CC2-Ex04/code.py
+++ CC2/CC2-Ex04/code.py
@@ -4,12 +4,15 @@ caller_number = randint(1, 10)
 # DO NOT TOUCH ============================================
 
 # YOUR CODE GOES HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-
-# YOUR CODE GOES HERE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-# Answer VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-
 if caller_number == 9:
     print('You are the lucky winner!')
 else:
     print('Better luck next time.')
+# YOUR CODE GOES HERE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+# Answer VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+#if caller_number == 9:
+ #   print('You are the lucky winner!')
+#else:
+ #   print('Better luck next time.')
