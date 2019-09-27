466a3a6b7543bab9af74131f3492f85cb7660bee
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-27 13:36:13 +0000

    Completed CC2-Ex17.

---
+++ CC2/CC2-Ex17/COPY.py
@@ -0,0 +1,7 @@
+grades = [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]
+
+lowest_so_far = 100
+
+# Your code goes here
+
+print('Lowest grade is', lowest_so_far)
--- CC2/CC2-Ex17/code.py
+++ CC2/CC2-Ex17/code.py
@@ -2,6 +2,8 @@ grades = [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]
 
 lowest_so_far = 100
 
-# Your code goes here
+for grade in grades:
+    if grade < lowest_so_far:
+        lowest_so_far = grade
 
 print('Lowest grade is', lowest_so_far)
