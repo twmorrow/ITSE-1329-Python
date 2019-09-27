109454ae99d43b1af6b52d4cf2ac23c45e941a8b
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-27 13:45:15 +0000

    Completed CC2-Ex16.

--- CC2/CC2-Ex16/code.py
+++ CC2/CC2-Ex16/code.py
@@ -2,8 +2,8 @@ grades = [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]
 
 highest_so_far = -1
 
-for ________ in ________:
-    if ________ > _________:
-        ________ = ________
+for grade in grades:
+    if grade > highest_so_far:
+        highest_so_far = grade
 
 print('Highest grade is', highest_so_far)
