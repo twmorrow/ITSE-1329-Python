69b88abc90287cf23e6280267837578c2dfe0928
twmorrow <twmorrow@mymail.tstc.edu>
2019-09-27 14:03:40 +0000

    Completed CC2-Ex18.

--- CC2/CC2-Ex18/COPY.py
+++ CC2/CC2-Ex18/COPY.py
@@ -1,5 +1,7 @@
 rain = [.25, .76, 1.2, 1.1, .03, .97, 2.3]
+
 rain_total = 0.0
-for inches in rain: 
-    rain_total = rain_total + inches
+
+# Your code here
+
 print('The total amount of rain was:', rain_total)
