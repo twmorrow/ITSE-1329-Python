9822154f7b8c5bc613de5d825c83302048a4db95
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-16 01:08:19 +0000

    Completed CC3-Ex07.

--- CC3/CC3-Ex07/code.py
+++ CC3/CC3-Ex07/code.py
@@ -1,2 +1,4 @@
 def my_first_function(fname, lname):
-    return ______________  # Fill in the blank
+    return ('The first value is {} and the second value is {}'.format(fname, lname))
+    
+print(my_first_function('john', 'doe'))
