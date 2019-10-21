08b4db0584d201e4389ccda793c246a1cae7ce3e
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-21 20:59:20 +0000

    Completed CC3-13.

--- CC3/CC3-Ex13/code.py
+++ CC3/CC3-Ex13/code.py
@@ -1,1 +1,9 @@
+#CC3_Ex13
 def not_string(string):
+    for word in string:
+        if (string[0:3]) == 'not':
+            return (string)
+        else:
+            return ('not ' + (string))
+            
+#print(not_string('bananas'))
