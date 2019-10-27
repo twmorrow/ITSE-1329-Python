587fbbd79c926730b6bb149c700247c4fbe6f0a4
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-27 13:40:01 +0000

    Completed CC4-Ex05.

--- CC4/CC4-Ex05/code.py
+++ CC4/CC4-Ex05/code.py
@@ -1,2 +1,9 @@
 #   open alice.txt, count the lines, and
 #   print out number of lines
+#CC4-Ex05
+with open('alice.txt', 'r') as file:
+    count = 0
+    for lines in file:
+        count = count + 1
+print('Line count: ' + str(count))
+    
