a1b3cf83a3cfae1c5a0245964ca8690478da4e42
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-27 13:44:58 +0000

    Completed CC4-Ex04.

---
+++ CC4/CC4-Ex04/COPY.py
@@ -0,0 +1,7 @@
+#CC4_Ex04
+with _____('mbox-short.txt', '__') as file:
+    count = 0
+    for _____ in _____:
+        count += 1  # this same as count = count + 1
+
+________(______)
--- CC4/CC4-Ex04/code.py
+++ CC4/CC4-Ex04/code.py
@@ -1,6 +1,7 @@
-with _____('mbox-short.txt', '__') as file:
+#CC4_Ex04
+with open('mbox-short.txt', 'r') as file:
     count = 0
-    for _____ in _____:
+    for lines in file:
         count += 1  # this same as count = count + 1
 
-________(______)
+print(count)
