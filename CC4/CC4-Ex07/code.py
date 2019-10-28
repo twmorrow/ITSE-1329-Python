7ffa5587fac84bcebb5dae313ac9908e5a0fe8bb
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-28 13:43:46 +0000

    Completed CC4-Ex07.

--- CC4/CC4-Ex07/code.py
+++ CC4/CC4-Ex07/code.py
@@ -1,3 +1,9 @@
 #   open alice.txt, print lines that startwith
 #   'Alice' and print out result with right-hand whitespace
 #   removed
+#CC4-Ex07
+work = open('alice.txt')
+for line in work:
+    line = line.rstrip()
+    if line.startswith('Alice'):
+        print(line)
