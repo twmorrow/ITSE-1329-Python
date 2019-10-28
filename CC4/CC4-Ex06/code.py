0eba12743b699ea8be0ac7324afd5098f951c9aa
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-28 13:24:34 +0000

    Completed CC4-Ex06.

--- CC4/CC4-Ex06/code.py
+++ CC4/CC4-Ex06/code.py
@@ -1,3 +1,10 @@
 #   open mbox-short.txt, print lines that startwith
 #   From: and print out result with right-hand whitespace
 #   removed
+#CC4-Ex06
+work = open('mbox-short.txt')
+for lines in work:
+    lines = lines.rstrip()
+    if lines.startswith('From:'):
+        print(lines)
+    
