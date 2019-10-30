533f222ea4c808be785fdfccd101f62917b63f2c
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-30 13:49:40 +0000

    Completed CC5-Ex04.

--- CC5/CC5-Ex04/code.py
+++ CC5/CC5-Ex04/code.py
@@ -1,9 +1,12 @@
 # DO NOT TOUCH
+#CC5-Ex04
 pets = ["Garey", "Santa Paws", "Clifferd", "buster", "Winnie the Poodle"]
 # DO NOT TOUCH
 
 #Change "Garey" to "Gary"
-
+pets[0] = 'Gary'
 #Change "Clifferd" to "Clifford"
-
+pets[2] = 'Clifford'
 #Change "buster" to "Buster" (Capitalize it)
+pets[3] = 'Buster'
+#print(pets)
