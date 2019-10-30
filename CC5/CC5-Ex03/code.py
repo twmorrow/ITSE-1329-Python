f3e7b14009517f951bbd471229b38f82c7576794
twmorrow <twmorrow@mymail.tstc.edu>
2019-10-30 13:42:48 +0000

    Completed CC5-Ex03.

--- CC5/CC5-Ex03/code.py
+++ CC5/CC5-Ex03/code.py
@@ -1,12 +1,14 @@
+#CC5-Ex03
 # DO NOT TOUCH
 stuff = [42, "pickle", 87.0]
 # DO NOT TOUCH
 
 #Change 42 to 50
-stuff[___] = _____
+stuff[0] = 50
 
 #Change "pickle" to "cucumber"
-stuff[___] = "______"
+stuff[1] = "cucumber"
 
 #Change 87.0 to 97.0
-stuff[___] = _____
+stuff[2] = 97.0
+#print(stuff)
