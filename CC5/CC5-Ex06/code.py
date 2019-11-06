95bf1d186dd89748d62f0ebd532207271df29931
twmorrow <twmorrow@mymail.tstc.edu>
2019-11-06 14:42:27 +0000

    Completed CC5-Ex06

--- CC5/CC5-Ex06/code.py
+++ CC5/CC5-Ex06/code.py
@@ -3,3 +3,8 @@ strings = ["Ses", "qui", "ped", "alian"]
 # DO NOT TOUCH
 
 # Write you code below
+result = ''
+for x in strings:
+    result = result + x
+    result = result.upper()
+print(result)
