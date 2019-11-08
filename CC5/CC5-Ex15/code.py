4c0c74d48551347584d0fb3994ddc5afd88fc382
twmorrow <twmorrow@mymail.tstc.edu>
2019-11-08 14:29:14 +0000

    Completed CC5-Ex15.

--- CC5/CC5-Ex15/code.py
+++ CC5/CC5-Ex15/code.py
@@ -1,5 +1,7 @@
+#CC5-Ex15
 def pet_names(names):
     counts = dict() # This is another way to create an empty dictionary
     for name in names:
-        counts[_____] = counts.get(_____, _____) + _____
+        counts[name] = counts.get(name, 0) + 1
     return counts
+print(pet_names(['fido', 'bob', 'joe', 'fido', 'fido']))
