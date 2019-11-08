4fb322fe345392500c6ca1d14b9fe9c7aa18f44a
twmorrow <twmorrow@mymail.tstc.edu>
2019-11-08 14:48:19 +0000

    Completed CC5-Ex17.

--- CC5/CC5-Ex17/code.py
+++ CC5/CC5-Ex17/code.py
@@ -1,1 +1,7 @@
+#CC5-Ex17
 def pet_nums(pets):
+    counts = dict()
+    for animal in pets:
+       counts[animal] = counts.get(animal, 0) + 1
+    return counts
+print(pet_nums(['cat', 'cat', 'cat', 'dog']))
